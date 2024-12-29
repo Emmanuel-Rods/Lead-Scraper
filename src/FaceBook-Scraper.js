3537966dcc91cbc84393362ca717073f6f884664
Emmanuel Rodrigues <alwesor@gmail.com>
2024-12-29 17:57:11 +0530

    fix notifications error

--- src/FaceBook-Scraper.js
+++ src/FaceBook-Scraper.js
@@ -115,8 +115,8 @@ async function facebookScraper(query , cityName) {
           return null;
         };
         
-        let nameElement = null
-        nameElement = document.querySelector("h1");
+        const allH1s = [...document.querySelectorAll("h1:not(.html-div h1)")];
+        const nameElement = allH1s.length ? allH1s[allH1s.length - 1] : null; // Get the last h1
         const name = nameElement ? nameElement.textContent.trim() : null;
 
         // Extract phone number, email, and website using their unique file names
