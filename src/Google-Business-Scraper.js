d7cd8dc8ee2ba231f138e782d9313e7999c2734f
Emmanuel Rodrigues <alwesor@gmail.com>
2025-01-01 21:50:51 +0530

    fix reloading bug

--- src/Google-Business-Scraper.js
+++ src/Google-Business-Scraper.js
@@ -11,7 +11,7 @@ puppeteerExtra.use(Stealth());
 
 async function gBusiness(service, location) {
   const query = `${service} in ${location}`;
-  const browser = await puppeteerExtra.launch({ headless: true});
+  const browser = await puppeteerExtra.launch({ headless: true });
   const page = await browser.newPage();
 
   await page.setViewport({
@@ -44,9 +44,27 @@ async function gBusiness(service, location) {
     throw new Error("Search bar not found");
   }
   await new Promise((resolve) => setTimeout(resolve, 2000));
+
   await page.reload({ waitUntil: "networkidle2" });
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  const isMapLoaded = async (page) => {
+    const isLoaded = await page.evaluate(() => {
+      const mapElement = document.querySelector('div.yXg2De[jsname="haAclf"]');
+      return mapElement && window.getComputedStyle(mapElement).opacity === '1';
+    });
+    return isLoaded;
+  };
+
   
+  const mapLoaded = await isMapLoaded(page);
+
+  if (!mapLoaded) {
+    console.info(`Map reloading...`)
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    await page.reload();
+    console.info(`Map reloaded`)
+  }
+
   let results = [];
 
   while (true) {
@@ -93,7 +111,9 @@ async function gBusiness(service, location) {
     // Break the loop
     if (!clicked) {
       console.log("No more pages to navigate");
-      console.info(`Extracted Data from Google Local Business ${results.length}`);
+      console.info(
+        `Extracted Data from Google Local Business ${results.length}`
+      );
       break;
     }
 
@@ -159,15 +179,15 @@ async function gBusiness(service, location) {
 
       const data = await page.evaluate(() => {
         // Extract text by matching an SVG icon
-        const extractInfoBySvgPath = (svgPath , match = 1) => {
+        const extractInfoBySvgPath = (svgPath, match = 1) => {
           const svgElements = document.querySelectorAll("svg");
           let matchCount = 0; // Counter to track matches
-      
+
           for (const svg of svgElements) {
             const pathElement = svg.querySelector("path");
             if (pathElement && pathElement.getAttribute("d") === svgPath) {
               matchCount++; // Increment match count
-      
+
               if (matchCount === match) {
                 const parentDiv = svg.parentElement;
                 if (parentDiv && parentDiv.nextElementSibling) {
@@ -178,11 +198,11 @@ async function gBusiness(service, location) {
           }
           return null; // Return null if the second match isn't found
         };
-      
+
         // Extract href by matching an SVG icon
         const extractLinkBySvgPath = (svgPath) => {
           const svgElements = document.querySelectorAll("svg");
-        
+
           for (const svg of svgElements) {
             const pathElement = svg.querySelector("path");
             if (pathElement && pathElement.getAttribute("d") === svgPath) {
@@ -195,27 +215,29 @@ async function gBusiness(service, location) {
           }
           return null; // Return null if no matching link is found
         };
-        
-       // SVG paths
-       // const phoneSvgPath = `M16.02 14.46l-2.62 2.62a16.141 16.141 0 0 1-6.5-6.5l2.62-2.62a.98.98 0 0 0 .27-.9L9.15 3.8c-.1-.46-.51-.8-.98-.8H4.02c-.56 0-1.03.47-1 1.03a17.92 17.92 0 0 0 2.43 8.01 18.08 18.08 0 0 0 6.5 6.5 17.92 17.92 0 0 0 8.01 2.43c.56.03 1.03-.44 1.03-1v-4.15c0-.48-.34-.89-.8-.98l-3.26-.65c-.33-.07-.67.04-.91.27z`;
+
+        // SVG paths
+        // const phoneSvgPath = `M16.02 14.46l-2.62 2.62a16.141 16.141 0 0 1-6.5-6.5l2.62-2.62a.98.98 0 0 0 .27-.9L9.15 3.8c-.1-.46-.51-.8-.98-.8H4.02c-.56 0-1.03.47-1 1.03a17.92 17.92 0 0 0 2.43 8.01 18.08 18.08 0 0 0 6.5 6.5 17.92 17.92 0 0 0 8.01 2.43c.56.03 1.03-.44 1.03-1v-4.15c0-.48-.34-.89-.8-.98l-3.26-.65c-.33-.07-.67.04-.91.27z`;
         const websiteSvgPath = `M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z`;
-        const addressSvgPath = `M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z`
+        const addressSvgPath = `M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z`;
 
-        const address = extractInfoBySvgPath(addressSvgPath) ; //textcontent
-        const websiteLink = extractLinkBySvgPath(websiteSvgPath , 3); //href 
+        const address = extractInfoBySvgPath(addressSvgPath); //textcontent
+        const websiteLink = extractLinkBySvgPath(websiteSvgPath, 3); //href
 
-        const phone = document.querySelector("div.eigqqc")?.textContent.trim() || null;
+        const phone =
+          document.querySelector("div.eigqqc")?.textContent.trim() || null;
         const name =
-          document.querySelector("div.rgnuSb.tZPcob")?.textContent.trim() || null;
+          document.querySelector("div.rgnuSb.tZPcob")?.textContent.trim() ||
+          null;
         const category =
           document.querySelector("div.bg3Wkc")?.textContent.trim() || null;
-      
+
         // Return collected data
         return {
           name: name || null,
           phone: phone || null,
           email: null,
-          website: websiteLink || null, 
+          website: websiteLink || null,
           address: address || null,
           category: category || null,
         };
